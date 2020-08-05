00bdb3e5bdb9ffa3c23b44f1d022421764600d29
Rumyana Dimitrieva <rumyanamm@gmail.com>
2020-08-05 16:15:21 -0700

    Updates

---
+++ precache-manifest.165756e00acf7ce2d2155a39e46795f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed3c4eb2e7b4e1abc70ed90d4c2f25e",
+    "url": "/monsters-test/index.html"
+  },
+  {
+    "revision": "b8e9062df9b3ab36a10c",
+    "url": "/monsters-test/static/css/main.353306fb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-test/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-test/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e9062df9b3ab36a10c",
+    "url": "/monsters-test/static/js/main.0957959c.chunk.js"
+  },
+  {
+    "revision": "c80b4e0c51a3a9ede362",
+    "url": "/monsters-test/static/js/runtime-main.deaaaa00.js"
+  }
+]);
